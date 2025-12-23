3948a8f58c0c0784a18a6db2f13f731a116f34ac
bala-amypo <bala@amypo.in>
2025-12-23 07:47:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/ServicePart.java
+++ src/main/java/com/example/demo/entity/ServicePart.java
@@ -22,8 +22,7 @@ public class ServicePart {
     private Integer quantity;
 
     public ServicePart() {}
-
-    // getters and setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
