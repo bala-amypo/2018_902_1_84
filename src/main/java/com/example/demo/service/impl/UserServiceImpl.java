3f99ee7ca63a0a5cfd5daa0208e0a12590bc067b
bala-amypo <bala@amypo.in>
2025-12-24 03:52:22 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,11 +1,25 @@
+package com.example.demo.service.impl;
+
 import com.example.demo.dto.AuthRequest;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
 
-public void login(AuthRequest request) {
-    String email = request.getEmail();
-    String password = request.getPassword();
-}
+@Service
+public class UserServiceImpl implements UserService {
 
-public void register(AuthRequest request) {
-    String email = request.getEmail();
-    String password = request.getPassword();
+    @Override
+    public void login(AuthRequest request) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        // TODO: login logic
+    }
+
+    @Override
+    public void register(AuthRequest request) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        // TODO: register logic
+    }
 }
