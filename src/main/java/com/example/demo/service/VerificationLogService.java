7aaa1550ddea27e0a164324ba8d9e7183b934bfb
bala-amypo <bala@amypo.in>
2025-12-26 10:10:25 +0000

    User commit

--- src/main/java/com/example/demo/service/VerificationLogService.java
+++ src/main/java/com/example/demo/service/VerificationLogService.java
@@ -4,10 +4,7 @@ import com.example.demo.model.VerificationLog;
 import java.util.List;
 
 public interface VerificationLogService {
-
     VerificationLog createLog(VerificationLog log);
-
-    VerificationLog getLogById(Long id);
-
     List<VerificationLog> getLogsForEntry(Long entryId);
+    VerificationLog getLogById(Long id);
 }
