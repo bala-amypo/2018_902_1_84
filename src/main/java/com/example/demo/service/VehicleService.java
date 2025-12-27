15940f110cc45dffbab8ec84e24a01224b56364c
bala-amypo <bala@amypo.in>
2025-12-27 05:13:39 +0000

    User commit

--- src/main/java/com/example/demo/service/VehicleService.java
+++ src/main/java/com/example/demo/service/VehicleService.java
@@ -1,12 +1,9 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Vehicle;
-import java.util.List;
+import com.example.demo.entity.Vehicle;
 
 public interface VehicleService {
-    Vehicle createVehicle(Vehicle vehicle);
+
     Vehicle getVehicleById(Long id);
-    Vehicle getVehicleByVin(String vin);
-    List<Vehicle> getVehiclesByOwner(Long ownerId);
-    void deactivateVehicle(Long id);
+
 }
--- src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
@@ -1,49 +1,25 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.entity.Vehicle;
 import com.example.demo.exception.EntityNotFoundException;
-import com.example.demo.model.Vehicle;
 import com.example.demo.repository.VehicleRepository;
 import com.example.demo.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 public class VehicleServiceImpl implements VehicleService {
+
     private final VehicleRepository vehicleRepository;
 
+    @Autowired
     public VehicleServiceImpl(VehicleRepository vehicleRepository) {
         this.vehicleRepository = vehicleRepository;
     }
 
     @Override
-    public Vehicle createVehicle(Vehicle vehicle) {
-        if (vehicleRepository.findByVin(vehicle.getVin()).isPresent()) {
-            throw new IllegalArgumentException("Duplicate VIN");
-        }
-        return vehicleRepository.save(vehicle);
-    }
-
-    @Override
     public Vehicle getVehicleById(Long id) {
-        return vehicleRepository.findById(id).orElse(null);
-    }
-
-
-    @Override
-    public Vehicle getVehicleByVin(String vin) {
-        return vehicleRepository.findByVin(vin)
+        return vehicleRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Vehicle not found"));
-    }
-
-    @Override
-    public List<Vehicle> getVehiclesByOwner(Long ownerId) {
-        return vehicleRepository.findByOwnerId(ownerId);
-    }
-
-    @Override
-    public void deactivateVehicle(Long id) {
-        Vehicle v = getVehicleById(id);
-        v.setActive(false);
-        vehicleRepository.save(v);
     }
 }
